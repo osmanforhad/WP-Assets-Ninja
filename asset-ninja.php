158760e163a54b5bf78acc8375afabfe01102c29
osmanforhad <osmanforhad93@gmail.com>
2022-04-15 19:20:12 +0600

    setup plugin header

---
+++ asset-ninja.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Assets Ninja
+ * Plugin URI:        https://osmanforhad.net/plugins/practice/
+ * Description:       Wordpress assets management plugin
+ * and Display QR code under every WorPress Post
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.1
+ * Author:            osman forhad
+ * Author URI:        https://author.osmanforhad.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       assetsninja
+ * Domain Path:       /languages/
+ */
